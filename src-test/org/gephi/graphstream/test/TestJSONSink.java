810a2f8b122c617b3e5e24addf05e8bca87383ca
Min WU <wumalbert@gmail.com>
2012-05-30 20:53:28 +0800

    First task, communicate from graphstream to gephi

---
+++ src-test/org/gephi/graphstream/test/TestJSONSink.java
@@ -0,0 +1,37 @@
+package org.gephi.graphstream.test;
+
+import org.gephi.graphstream.JSONSink;
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.GridGenerator;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.SourceBase;
+
+public class TestJSONSink {
+	public static void main(String args[]) {
+	
+		Graph graph = new SingleGraph("grid");
+		Generator gen = new GridGenerator();
+		
+		//Nodes already have a position.
+		//graph.display(false);
+		
+		JSONSink jsonSink = new JSONSink();
+		graph.addSink(jsonSink);
+		
+		gen.addSink(graph);
+		gen.begin();
+		for(int i=0; i<4; i++) {
+		    gen.nextEvents();
+		    sleep();
+		}
+		gen.end(); 
+		
+	}
+	
+	protected static void sleep() {
+        try {
+        	Thread.sleep(1000); 
+        } catch (Exception e) {}
+    }
+}
